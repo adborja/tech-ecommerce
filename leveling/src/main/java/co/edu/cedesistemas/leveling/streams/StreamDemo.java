6f0e09fa6d9a60603ab1c978e31b0867d9ea95ed
Edward Alexis Ortiz <edaortiz@bancolombia.com.co>
2020-07-02 18:09:12 -0500

    se solucionan las pruebas fallidas con streams

--- leveling/src/main/java/co/edu/cedesistemas/leveling/streams/StreamDemo.java
+++ leveling/src/main/java/co/edu/cedesistemas/leveling/streams/StreamDemo.java
@@ -1,19 +1,22 @@
 package co.edu.cedesistemas.leveling.streams;
 
 
+import co.edu.cedesistemas.leveling.functional.ShapeMultiplier;
+import co.edu.cedesistemas.leveling.model.geometry.Circle;
 import co.edu.cedesistemas.leveling.model.geometry.Scalable;
 import co.edu.cedesistemas.leveling.model.geometry.Shape;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamDemo {
     /**
      * Retorna una lista filtrada con las figuras que tienen un área menor o igual a límite de área especificado.
      *
      * @param areaLimit El valor máximo del área.
-     * @param shapes Lista de figuras
+     * @param shapes    Lista de figuras
      * @return Lista filtrada con las figuras que tienen un área menor o igual a la especificada.
-     * */
+     */
     public static List<? extends Shape> filterShapes(double areaLimit, List<? extends Shape> shapes) {
         return null;
     }
@@ -22,10 +25,11 @@ public class StreamDemo {
      * Retorna una lista de objetos escalables escalados por un valor especificado.
      *
      * @param scalables Lista de objetos escalables.
-     * @param value Valor numérico a escalar.
+     * @param value     Valor numérico a escalar.
      * @return Lista con objetos escalados
-     * */
+     */
     public static <T extends Scalable<T, U>, U extends Number> List<T> scale(List<T> scalables, U value) {
-        return null;
+        ShapeMultiplier<T, U> shapeMultiplier = (c, v) -> c.scale(v);
+        return scalables.stream().map(t -> shapeMultiplier.multiply(t, value)).collect(Collectors.toList());
     }
 }
