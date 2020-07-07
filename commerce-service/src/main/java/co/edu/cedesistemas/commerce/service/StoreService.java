cf97a592abf1c1ff49af591a9a0d820b152c9d70
adborja <adborja@gmail.com>
2020-07-07 15:07:46 -0500

    Removing StoreService functionalities

--- commerce-service/src/main/java/co/edu/cedesistemas/commerce/service/StoreService.java
+++ commerce-service/src/main/java/co/edu/cedesistemas/commerce/service/StoreService.java
@@ -2,9 +2,7 @@ package co.edu.cedesistemas.commerce.service;
 
 import co.edu.cedesistemas.commerce.model.Store;
 import co.edu.cedesistemas.commerce.repository.StoreRepository;
-import co.edu.cedesistemas.common.util.Utils;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -23,23 +21,18 @@ public class StoreService {
     }
 
     public Store getById(final String id) {
-        return repository.findById(id).orElse(null);
+        return null;
     }
 
     public List<Store> getByType(final Store.Type type) {
-        return repository.findByType(type);
+        return null;
     }
 
     public List<Store> getByName(final String name) {
-        return repository.findByNameLike(name);
+        return null;
     }
 
     public Store updateStore(String id, Store store) {
-        Store found = getById(id);
-        if (found == null) {
-            return null;
-        }
-        BeanUtils.copyProperties(store, found, Utils.getNullPropertyNames(store));
-        return repository.save(found);
+        return null;
     }
 }
