2a8e6ff6bff9f86e3dd5aae399f05889e5fac681
Estefania Higuita Molina <ehmolin@bancolombia.com.co>
2020-07-07 12:20:52 -0500

    Segundo punto taller reto 1

--- leveling/src/main/java/co/edu/cedesistemas/leveling/functional/FunctionHandler.java
+++ leveling/src/main/java/co/edu/cedesistemas/leveling/functional/FunctionHandler.java
@@ -3,6 +3,7 @@ package co.edu.cedesistemas.leveling.functional;
 import co.edu.cedesistemas.leveling.model.geometry.Scalable;
 import co.edu.cedesistemas.leveling.model.geometry.Shape;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -10,18 +11,28 @@ import java.util.function.Function;
 
 public class FunctionHandler {
     public static <T, R> R applyFunction(Function<T, R> f, T t) {
-        return null;
+
+        return f.apply(t);
     }
 
     public static <T extends Scalable<T, U>, U extends Number> T getMultiplied(ShapeMultiplier<T, U> f, T shape, U value) {
-        return null;
+        return f.multiply(shape,value);
     }
 
     public static <T> void consume(Consumer<List<T>> consumer, List<T> list) {
-
+        consumer.accept(list);
     }
 
     public static Function<List<? extends Shape>, Map<? extends Shape, Double>> getAreaMapper() {
-        return null;
+
+        return (l) -> {
+            Map <Shape, Double> areas = new HashMap<>();
+            for(Shape s : l) {
+                Double area = s.area();
+                areas.put(s, area);
+            }
+            return areas;
+        };
+
     }
 }
