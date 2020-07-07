0bc4bc4de540bb2718ad9a676f4f732a0828d517
adborja <adborja@gmail.com>
2020-07-07 15:05:43 -0500

    Adding Integration Test Cases. Adding SpringBoot support. Adding Mongo support.

---
+++ commerce-service/src/main/java/co/edu/cedesistemas/commerce/controller/StoreController.java
@@ -0,0 +1,78 @@
+package co.edu.cedesistemas.commerce.controller;
+
+import co.edu.cedesistemas.commerce.model.Store;
+import co.edu.cedesistemas.commerce.service.StoreService;
+import co.edu.cedesistemas.common.DefaultResponseBuilder;
+import co.edu.cedesistemas.common.model.Status;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+public class StoreController {
+    private final StoreService service;
+
+    @PostMapping("/stores")
+    public ResponseEntity<Status<?>> createStore(@RequestBody Store store) {
+        try {
+            Store created = service.createStore(store);
+            return DefaultResponseBuilder.defaultResponse(created, HttpStatus.CREATED);
+        } catch (Exception ex) {
+            return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/stores/{id}")
+    public ResponseEntity<Status<?>> getStoreById(@PathVariable String id) {
+        try {
+            Store found = service.getById(id);
+            if (found != null) return DefaultResponseBuilder.defaultResponse(found, HttpStatus.OK);
+            else return DefaultResponseBuilder.errorResponse("store not found", null, HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/stores/by-name")
+    public ResponseEntity<Status<?>> getStoresByName(@RequestParam String name) {
+        try {
+            List<Store> found = service.getByName(name);
+            return DefaultResponseBuilder.defaultResponse(found, HttpStatus.OK);
+        } catch (Exception ex) {
+            return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/stores/by-type")
+    public ResponseEntity<Status<?>> getStoresByType(@RequestParam Store.Type type) {
+        try {
+            List<Store> found = service.getByType(type);
+            return DefaultResponseBuilder.defaultResponse(found, HttpStatus.OK);
+        } catch (Exception ex) {
+            return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PatchMapping("/stores/{id}")
+    public ResponseEntity<Status<?>> updateStore(@PathVariable String id, @RequestBody Store store) {
+        try {
+            Store updated = service.updateStore(id, store);
+            if (updated != null) return DefaultResponseBuilder.defaultResponse(updated, HttpStatus.OK);
+            else return DefaultResponseBuilder.errorResponse("store not found", null, HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- commerce-service/src/main/java/co/edu/cedesistemas/commerce/service/StoreService.java
+++ commerce-service/src/main/java/co/edu/cedesistemas/commerce/service/StoreService.java
@@ -2,7 +2,9 @@ package co.edu.cedesistemas.commerce.service;
 
 import co.edu.cedesistemas.commerce.model.Store;
 import co.edu.cedesistemas.commerce.repository.StoreRepository;
+import co.edu.cedesistemas.common.util.Utils;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -20,7 +22,24 @@ public class StoreService {
         return repository.save(store);
     }
 
+    public Store getById(final String id) {
+        return repository.findById(id).orElse(null);
+    }
+
     public List<Store> getByType(final Store.Type type) {
         return repository.findByType(type);
+    }
+
+    public List<Store> getByName(final String name) {
+        return repository.findByNameLike(name);
+    }
+
+    public Store updateStore(String id, Store store) {
+        Store found = getById(id);
+        if (found == null) {
+            return null;
+        }
+        BeanUtils.copyProperties(store, found, Utils.getNullPropertyNames(store));
+        return repository.save(found);
     }
 }
