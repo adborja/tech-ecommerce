7bf260ee3a1951d030f42a7b0437b89241f11e36
Juan Pablo Gaviria <jgavirias13@gmail.com>
2020-07-08 23:41:26 -0500

    Order Test
    Se implementa el servicio y el controlador de orden para los test

---
+++ commerce-service/src/main/java/co/edu/cedesistemas/commerce/controller/OrderController.java
@@ -0,0 +1,51 @@
+package co.edu.cedesistemas.commerce.controller;
+
+import co.edu.cedesistemas.commerce.model.Order;
+import co.edu.cedesistemas.commerce.model.OrderItem;
+import co.edu.cedesistemas.commerce.service.OrderService;
+import co.edu.cedesistemas.common.DefaultResponseBuilder;
+import co.edu.cedesistemas.common.model.Status;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+public class OrderController {
+    private final OrderService service;
+
+    @PostMapping("/orders")
+    public ResponseEntity<Status<?>> createOrder(@RequestBody Order order){
+        try{
+            Order created = service.createOrder(order);
+            return DefaultResponseBuilder.defaultResponse(created, HttpStatus.CREATED);
+        }catch (Exception e){
+            return DefaultResponseBuilder.errorResponse(e.getMessage(), e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/orders/{id}")
+    public ResponseEntity<Status<?>> getOrderById(@PathVariable String id){
+        try{
+            Order order = service.getById(id);
+            return DefaultResponseBuilder.defaultResponse(order, HttpStatus.OK);
+        }catch (Exception e){
+            return DefaultResponseBuilder.errorResponse(e.getMessage(), e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/orders/{id}/items")
+    public ResponseEntity<Status<?>> getOrderItems(@PathVariable String id){
+        try{
+            List<OrderItem> items = service.getItems(id);
+            return DefaultResponseBuilder.defaultResponse(items, HttpStatus.OK);
+        }catch (Exception e){
+            return DefaultResponseBuilder.errorResponse(e.getMessage(), e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ commerce-service/src/main/java/co/edu/cedesistemas/commerce/service/OrderService.java
@@ -0,0 +1,34 @@
+package co.edu.cedesistemas.commerce.service;
+
+import co.edu.cedesistemas.commerce.model.Order;
+import co.edu.cedesistemas.commerce.model.OrderItem;
+import co.edu.cedesistemas.commerce.repository.OrderRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class OrderService {
+    private final OrderRepository repository;
+
+    public Order createOrder(final Order order){
+        order.setId(UUID.randomUUID().toString());
+        order.setStatus(Order.Status.CREATED);
+        return repository.save(order);
+    }
+
+    public Order getById(final String id){
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<OrderItem> getItems(final String id){
+        Order order = getById(id);
+        if(order == null){
+            return null;
+        }
+        return getById(id).getItems();
+    }
+}
