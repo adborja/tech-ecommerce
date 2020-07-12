9c153832012ac7e23d14f43cc83d7bd34877f1b7
Edward Alexis Ortiz <edaortiz@bancolombia.com.co>
2020-07-12 15:36:44 -0500

    se agrega los links al controller de address

--- commerce-service/src/main/java/co/edu/cedesistemas/commerce/controller/AddressesController.java
+++ commerce-service/src/main/java/co/edu/cedesistemas/commerce/controller/AddressesController.java
@@ -5,12 +5,17 @@ import co.edu.cedesistemas.commerce.service.AddressService;
 import co.edu.cedesistemas.common.DefaultResponseBuilder;
 import co.edu.cedesistemas.common.model.Status;
 import lombok.AllArgsConstructor;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotNull;
 import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @AllArgsConstructor
@@ -33,12 +38,21 @@ public class AddressesController {
         try {
             Optional<Address> address = this.addressService.findById(id);
             if (address.isPresent()) {
+                addSelfLink(address.get());
                 return DefaultResponseBuilder.defaultResponse(address.get(), HttpStatus.OK);
             }
             return DefaultResponseBuilder.errorResponse("Address not found", null, HttpStatus.NOT_FOUND);
         } catch (Exception ex) {
             return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    private static void addSelfLink(@NotNull Address address) {
+        Link selfLink = linkTo(methodOn(AddressesController.class)
+                .getAddressById(address.getId()))
+                .withSelfRel()
+                .withType("GET");
+        address.add(selfLink);
     }
 
 }
