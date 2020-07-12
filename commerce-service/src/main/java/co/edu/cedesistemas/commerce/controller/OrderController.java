a8b38b0a2133325129902a0f56bcdd870fdcbee8
kathe20m <kathe20m@hotmail.com>
2020-07-11 22:38:22 -0500

    taller Leccion 3 - springBoot

---
+++ commerce-service/src/main/java/co/edu/cedesistemas/commerce/controller/OrderController.java
@@ -0,0 +1,57 @@
+package co.edu.cedesistemas.commerce.controller;
+
+import co.edu.cedesistemas.commerce.model.Order;
+import co.edu.cedesistemas.commerce.model.OrderItem;
+import co.edu.cedesistemas.commerce.service.OrderService;
+import co.edu.cedesistemas.common.DefaultResponseBuilder;
+import co.edu.cedesistemas.common.model.Status;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+public class OrderController {
+    private final OrderService service;
+
+    @PostMapping("/orders")
+    public ResponseEntity<Status<?>> createOrder(@RequestBody Order order) {
+        try {
+            Order created = service.createOrder(order);
+            return DefaultResponseBuilder.defaultResponse(order, HttpStatus.CREATED);
+        } catch (Exception ex) {
+            return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/orders/{id}")
+    public ResponseEntity<Status<?>> getOrderById(@PathVariable String id) {
+        try {
+            Order found = service.getById(id);
+            if (found != null) return DefaultResponseBuilder.defaultResponse(found, HttpStatus.OK);
+            else return DefaultResponseBuilder.defaultResponse("order not found", HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/orders/{id}/items")
+    public ResponseEntity<Status<?>> getOrderItems(@PathVariable String id) {
+        try {
+
+            List<OrderItem> found = service.getOrderItems(id);
+            if (found != null) return DefaultResponseBuilder.defaultResponse(found, HttpStatus.OK);
+            else return DefaultResponseBuilder.errorResponse("store not found", null, HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+}
---
+++ commerce-service/src/main/java/co/edu/cedesistemas/commerce/service/OrderService.java
@@ -0,0 +1,33 @@
+package co.edu.cedesistemas.commerce.service;
+
+import co.edu.cedesistemas.commerce.model.Order;
+import co.edu.cedesistemas.commerce.model.OrderItem;
+import co.edu.cedesistemas.commerce.repository.OrderRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class OrderService  {
+
+    private final OrderRepository repository;
+
+    public Order createOrder(final Order order) {
+        order.setStatus(Order.Status.CREATED);
+
+        return repository.save(order);
+    }
+
+    public Order getById(final String id) {
+        return repository.findById(id).get();
+    }
+
+    public List<OrderItem> getOrderItems(String id){
+        Optional<Order> order = repository.findById(id);
+        List<OrderItem> orderItems = order.get().getItems();
+        return orderItems;
+    }
+}
