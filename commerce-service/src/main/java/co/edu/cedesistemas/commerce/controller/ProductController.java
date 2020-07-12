fbbb2af27bb2764d47e33753a12e62e5ff98d9cf
Edward Alexis Ortiz <edaortiz@bancolombia.com.co>
2020-07-12 14:54:50 -0500

    se agregan los links hateoas a la clase productoController

--- commerce-service/src/main/java/co/edu/cedesistemas/commerce/controller/ProductController.java
+++ commerce-service/src/main/java/co/edu/cedesistemas/commerce/controller/ProductController.java
@@ -5,15 +5,20 @@ import co.edu.cedesistemas.commerce.service.ProductService;
 import co.edu.cedesistemas.common.DefaultResponseBuilder;
 import co.edu.cedesistemas.common.model.Status;
 import lombok.AllArgsConstructor;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotNull;
 import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @AllArgsConstructor
@@ -25,6 +30,8 @@ public class ProductController {
     public ResponseEntity<Status<?>> createProduct(@RequestBody Product product) {
         try {
             Product created = this.productService.createProduct(product);
+            addSelfLink(created);
+            addLinks(created);
             return DefaultResponseBuilder.defaultResponse(created, HttpStatus.CREATED);
         } catch (Exception ex) {
             return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -32,13 +39,13 @@ public class ProductController {
     }
 
     @GetMapping("/products/{id}")
-    public ResponseEntity<Status<?>> getProducts(@PathVariable String id) {
+    public ResponseEntity<Status<?>> getProductById(@PathVariable String id) {
         try {
             Optional<Product> foundProduct = this.productService.findById(id);
-
             if (foundProduct.isPresent()) {
-                List<Product> products = List.of(foundProduct.get());
-                return DefaultResponseBuilder.defaultResponse(products, HttpStatus.OK);
+                Product newProduct = foundProduct.get();
+                addSelfLink(newProduct);
+                return DefaultResponseBuilder.defaultResponse(newProduct, HttpStatus.OK);
             }
             return DefaultResponseBuilder.errorResponse("Not found", null, HttpStatus.NOT_FOUND);
         } catch (Exception ex) {
@@ -51,6 +58,7 @@ public class ProductController {
         try {
             List<Product> products = this.productService.getProductByNamePrefix(name);
             if (products.size() > 0) {
+                products.forEach(ProductController::addSelfLink);
                 return DefaultResponseBuilder.defaultResponse(products, HttpStatus.OK);
             }
             return DefaultResponseBuilder.errorResponse("Product not found", null, HttpStatus.NOT_FOUND);
@@ -69,7 +77,9 @@ public class ProductController {
             Optional<Product> updatedProduct = this.productService.updateProduct(id, product);
 
             if (updatedProduct.isPresent()) {
-                return DefaultResponseBuilder.defaultResponse(updatedProduct.get(), HttpStatus.OK);
+                Product newProduct = updatedProduct.get();
+                addSelfLink(newProduct);
+                return DefaultResponseBuilder.defaultResponse(newProduct, HttpStatus.OK);
             }
             return DefaultResponseBuilder.errorResponse("Couldn't update product", null, HttpStatus.INTERNAL_SERVER_ERROR);
         } catch (Exception ex) {
@@ -88,5 +98,33 @@ public class ProductController {
         } catch (Exception ex) {
             return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    private static void addSelfLink(@NotNull final Product product) {
+        Link selfLink = linkTo(methodOn(ProductController.class).getProductById(
+                product.getId())).withSelfRel().withType("GET");
+        product.add(selfLink);
+    }
+
+    private static void addLinks(@NotNull final Product product) {
+        Link linkByName = linkTo(methodOn(ProductController.class).
+                getProductByName(product.getName()))
+                .withRel("by-name")
+                .withType("GET");
+        product.add(linkByName);
+
+        Link linkUpdateProduct = linkTo(methodOn(ProductController.class)
+                .updateProduct(product.getId(), product))
+                .withRel("update")
+                .withMedia(MediaType.APPLICATION_JSON_VALUE)
+                .withType("PATH");
+        product.add(linkUpdateProduct);
+
+        Link deleteProduct = linkTo(methodOn(ProductController.class)
+                .deleteProduct(product.getId()))
+                .withRel("delete")
+                .withType("DELETE");
+        product.add(deleteProduct);
+
     }
 }
