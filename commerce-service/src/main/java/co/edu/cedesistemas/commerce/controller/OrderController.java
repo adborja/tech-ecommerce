f7f3b665ff23e53f3b5697967e9ef7a4dab5654a
Edward Alexis Ortiz <edaortiz@bancolombia.com.co>
2020-07-12 15:34:23 -0500

    se agrega los links de hateoas a la clase order

--- commerce-service/src/main/java/co/edu/cedesistemas/commerce/controller/OrderController.java
+++ commerce-service/src/main/java/co/edu/cedesistemas/commerce/controller/OrderController.java
@@ -5,12 +5,17 @@ import co.edu.cedesistemas.commerce.service.OrderService;
 import co.edu.cedesistemas.common.DefaultResponseBuilder;
 import co.edu.cedesistemas.common.model.Status;
 import lombok.AllArgsConstructor;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotNull;
 import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @AllArgsConstructor
@@ -23,6 +28,8 @@ public class OrderController {
         try {
             order.setStatus(Order.Status.CREATED);
             Order created = this.orderService.createOrder(order);
+            addLinks(created);
+            addSelfLink(created);
             return DefaultResponseBuilder.defaultResponse(created, HttpStatus.CREATED);
         } catch (Exception ex) {
             return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -35,6 +42,7 @@ public class OrderController {
             Optional<Order> order = this.orderService.findById(id);
 
             if (order.isPresent()) {
+                addSelfLink(order.get());
                 return DefaultResponseBuilder.defaultResponse(order.get(), HttpStatus.OK);
             }
             return DefaultResponseBuilder.errorResponse("Order not found", null, HttpStatus.NOT_FOUND);
@@ -49,11 +57,28 @@ public class OrderController {
             Optional<Order> order = this.orderService.findById(id);
 
             if (order.isPresent()) {
+                addSelfLink(order.get());
                 return DefaultResponseBuilder.defaultResponse(order.get().getItems(), HttpStatus.OK);
             }
             return DefaultResponseBuilder.errorResponse("Order not found", null, HttpStatus.NOT_FOUND);
         } catch (Exception ex) {
             return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    private static void addSelfLink(@NotNull Order order) {
+        Link selfLink = linkTo(methodOn(OrderController.class)
+                .getOrderById(order.getId()))
+                .withSelfRel()
+                .withType("GET");
+        order.add(selfLink);
+    }
+
+    private static void addLinks(@NotNull Order order) {
+        Link linkItems = linkTo(methodOn(OrderController.class)
+                .getOrderItems(order.getId()))
+                .withRel("items")
+                .withType("GET");
+        order.add(linkItems);
     }
 }
