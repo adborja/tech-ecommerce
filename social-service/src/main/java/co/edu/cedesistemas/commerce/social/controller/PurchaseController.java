07bca2e800e60bf607c07bcc8dfe077fe01d0c83
adborja <adborja@gmail.com>
2020-07-13 15:36:52 -0500

    Fixing PurchaseController URI

--- social-service/src/main/java/co/edu/cedesistemas/commerce/social/controller/PurchaseController.java
+++ social-service/src/main/java/co/edu/cedesistemas/commerce/social/controller/PurchaseController.java
@@ -6,13 +6,22 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Set;
 
 @AllArgsConstructor
 public class PurchaseController {
     private final PurchaseService service;
 
-    @PutMapping("/purchases/users/{id}/products/{productId}/purchase")
+    @PutMapping("/purchases/users/{id}/products/{productId}")
     public ResponseEntity<Status<?>> purchase(@PathVariable String id, @PathVariable String productId) {
+        // TODO: Implement method here
+        return null;
+    }
+
+    @PutMapping("/purchases/users/{id}")
+    public ResponseEntity<Status<?>> purchase(@PathVariable String id, @RequestParam Set<String> productIds) {
         // TODO: Implement method here
         return null;
     }
