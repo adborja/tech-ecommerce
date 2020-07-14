8be1b8e0f2817e72694747500db7489b755fe9d3
adborja <adborja@gmail.com>
2020-07-14 16:00:16 -0500

    Adding @RestController annotation to controller classes. Changing neo4j hostname in application.yml Adding shell to run redis db.

--- social-service/src/main/java/co/edu/cedesistemas/commerce/social/controller/StoreController.java
+++ social-service/src/main/java/co/edu/cedesistemas/commerce/social/controller/StoreController.java
@@ -2,8 +2,10 @@ package co.edu.cedesistemas.commerce.social.controller;
 
 import co.edu.cedesistemas.commerce.social.model.Store;
 import co.edu.cedesistemas.commerce.social.service.StoreService;
+import co.edu.cedesistemas.common.DefaultResponseBuilder;
 import co.edu.cedesistemas.common.model.Status;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,15 +13,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @AllArgsConstructor
 public class StoreController {
     private final StoreService service;
 
     @PostMapping("/stores")
     public ResponseEntity<Status<?>> createStore(@RequestBody Store store) {
-        // TODO: Implement method here
-        return null;
+        Store created = service.createStore(store);
+        return DefaultResponseBuilder.defaultResponse(created, HttpStatus.CREATED);
     }
 
     @PutMapping("/stores/{id}/products/{productId}")
