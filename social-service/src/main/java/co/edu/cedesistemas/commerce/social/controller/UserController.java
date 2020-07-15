f7d30b9699f142acc12237b38fd40926d1d685ec
slotero <slotero@bancolombia.com.co>
2020-07-15 10:03:16 -0500

    Anotacion de restcontroller para los demás controladores

--- social-service/src/main/java/co/edu/cedesistemas/commerce/social/controller/UserController.java
+++ social-service/src/main/java/co/edu/cedesistemas/commerce/social/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 @AllArgsConstructor
 public class UserController {
     private final UserService service;
