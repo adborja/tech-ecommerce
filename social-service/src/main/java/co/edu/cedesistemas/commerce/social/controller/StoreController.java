d398e2777e7923e3809d7d9d2fd62e797668f3fc
adborja <adborja@gmail.com>
2020-07-15 13:37:37 -0500

    Adding spring-cloud-dependencies Adding discovery-service and gateway-service Adding eureka client dependency to commerce-service Adding actuator to commerce-service. Adding eureka client dependency to social-service Adding actuator to social-service. Adding HATEOAS support to StoreController in social-service. Changing neo4j hostname in application.yml Adding shell to run redis db.

--- social-service/src/main/java/co/edu/cedesistemas/commerce/social/controller/StoreController.java
+++ social-service/src/main/java/co/edu/cedesistemas/commerce/social/controller/StoreController.java
@@ -5,6 +5,8 @@ import co.edu.cedesistemas.commerce.social.service.StoreService;
 import co.edu.cedesistemas.common.DefaultResponseBuilder;
 import co.edu.cedesistemas.common.model.Status;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,14 +17,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotNull;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @AllArgsConstructor
+@Slf4j
 public class StoreController {
     private final StoreService service;
 
     @PostMapping("/stores")
     public ResponseEntity<Status<?>> createStore(@RequestBody Store store) {
         Store created = service.createStore(store);
+        addSelfLink(created);
         return DefaultResponseBuilder.defaultResponse(created, HttpStatus.CREATED);
     }
 
@@ -32,10 +41,34 @@ public class StoreController {
         return null;
     }
 
+    @GetMapping("/stores/{id}")
+    public ResponseEntity<Status<?>> getStoreById(@PathVariable String id) {
+        Store found = service.getById(id);
+        if (found != null) {
+            addLinks(found);
+            return DefaultResponseBuilder.defaultResponse(found, HttpStatus.OK);
+        } else return DefaultResponseBuilder.errorResponse("store not found", null, HttpStatus.NOT_FOUND);
+    }
+
     @GetMapping("/stores/{storeId}/products/top")
     public ResponseEntity<Status<?>> getTopNProducts(@PathVariable String storeId,
                                           @RequestParam(required = false, defaultValue = "5") Integer limit) {
         // TODO: Implement method here
         return null;
+    }
+
+    private static void addSelfLink(@NotNull final Store store) {
+        Link selfLink = linkTo(methodOn(StoreController.class)
+                .getStoreById(store.getId()))
+                .withSelfRel().withType("GET");
+        store.add(selfLink);
+    }
+
+    private static void addLinks(@NotNull final Store store) {
+        Link byTypeLink = linkTo(methodOn(StoreController.class)
+                .getTopNProducts(store.getId(), 5))
+                .withRel("top-5-products")
+                .withType("GET");
+        store.add(byTypeLink);
     }
 }
