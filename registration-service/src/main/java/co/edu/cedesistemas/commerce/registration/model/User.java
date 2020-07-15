c8a06888a4a51c28f733ef9fc0252291e3c99699
adborja <adborja@gmail.com>
2020-07-15 15:53:20 -0500

    Adding Gateway Integration Tests

---
+++ registration-service/src/main/java/co/edu/cedesistemas/commerce/registration/model/User.java
@@ -0,0 +1,34 @@
+package co.edu.cedesistemas.commerce.registration.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(of = "id")
+@Document
+public class User {
+    @Id
+    private String id;
+    private String name;
+    private String lastName;
+    private String address;
+    private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    private LocalDate birthday;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Status status;
+
+    public enum Status {
+        ACTIVE,
+        INACTIVE,
+        BLOCKED
+    }
+}
