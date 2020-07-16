b49c68a8a5e92204cbd38cebeb520b85319bfedd
Edward Alexis Ortiz <edaortiz@bancolombia.com.co>
2020-07-16 14:00:59 -0500

    se crea base del controller ActuatorController

---
+++ registration-service/src/main/java/co/edu/cedesistemas/commerce/registration/controller/ActuatorController.java
@@ -0,0 +1,7 @@
+package co.edu.cedesistemas.commerce.registration.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ActuatorController {
+}
