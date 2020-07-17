6612065f752c39c0cc5c7ccf158e409ea4f7dfd5
Edward Alexis Ortiz <alexixsortizz@gmail.com>
2020-07-17 14:12:37 -0500

    Solved ActuatorController tests
    *Actuator starter dependency added to the build.gradle file
    *ActuatorController class removed for unnecessary usage
    *Aplication-test.yaml file added for testing purposes
    *Application.yaml added with required properties to pass the unit tests

--- registration-service/src/main/java/co/edu/cedesistemas/commerce/registration/controller/ActuatorController.java
+++
@@ -1,7 +0,0 @@
-package co.edu.cedesistemas.commerce.registration.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ActuatorController {
-}
