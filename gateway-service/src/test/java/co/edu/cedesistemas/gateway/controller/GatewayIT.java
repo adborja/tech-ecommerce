a5afe6f8508163d912d20df68f881f6c023b8bbb
Edward Alexis Ortiz <alexixsortizz@gmail.com>
2020-07-17 17:07:37 -0500

    Solved gateway unit tests
    *Added registration route in the gateway properties file zuul routes
    *Commented @SpringBootTest(classes = GatewayApp.class) to start gateway manually 
    *Registration default port changed to 8083

--- gateway-service/src/test/java/co/edu/cedesistemas/gateway/controller/GatewayIT.java
+++ gateway-service/src/test/java/co/edu/cedesistemas/gateway/controller/GatewayIT.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = GatewayApp.class)
+//@SpringBootTest(classes = GatewayApp.class)
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class GatewayIT {
