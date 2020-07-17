1bfcd7e8034f4674a06c3fca5db033eb9904af0e
adborja <adborja@gmail.com>
2020-07-17 13:15:57 -0500

    Downgrading SpringCloudVersion to Hoxton.SR4 due Hystrix Dashboard jquery issues. Adding Hystrix Support. Adding Hystrix Command sample.

--- commerce-service/src/main/java/co/edu/cedesistemas/commerce/service/IStoreService.java
+++ commerce-service/src/main/java/co/edu/cedesistemas/commerce/service/IStoreService.java
@@ -7,11 +7,11 @@ import java.util.List;
 public interface IStoreService {
     Store createStore(Store store);
 
-    Store getById(String id);
+    Store getById(String id) throws Exception;
 
     List<Store> getByType(Store.Type type);
 
     List<Store> getByName(String name);
 
-    Store updateStore(String id, Store store) throws Exception;
+    Store updateStore(String id, Store store);
 }
