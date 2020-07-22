f01eee5b9df30284d760c43ef0fb21872c7c21b2
adborja <adborja@gmail.com>
2020-07-22 16:02:30 -0500

    Adding run_rabbitmq.sh.

--- commerce-service/src/main/java/co/edu/cedesistemas/commerce/event/OrderEvent.java
+++ commerce-service/src/main/java/co/edu/cedesistemas/commerce/event/OrderEvent.java
@@ -1,4 +1,30 @@
 package co.edu.cedesistemas.commerce.event;
 
+import co.edu.cedesistemas.commerce.model.Order;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class OrderEvent {
+    private String id; // order id
+    private String storeId;
+    private String userId;
+    private LocalDateTime createdAt;
+    private Order.Status status;
+
+    @Override
+    public String toString() {
+        return toJSON();
+    }
+
+    public String toJSON() {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(this);
+        } catch (Exception ex) {
+            return "{}";
+        }
+    }
 }
