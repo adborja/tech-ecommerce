23ff974fd707e0c6793536f6fb869fbd8792d304
Edward Alexis Ortiz <alexixsortizz@gmail.com>
2020-07-27 15:49:49 -0500

    I added logs to UserController of the service ecommerce
    *Added logs to the methods of the UserController of the service ecommerce
    *Added fallback methods to the UserController of the service ecommerce

--- commerce-service/src/main/java/co/edu/cedesistemas/commerce/controller/UserController.java
+++ commerce-service/src/main/java/co/edu/cedesistemas/commerce/controller/UserController.java
@@ -2,9 +2,9 @@ package co.edu.cedesistemas.commerce.controller;
 
 import co.edu.cedesistemas.commerce.model.User;
 import co.edu.cedesistemas.commerce.service.IUserService;
-import co.edu.cedesistemas.commerce.service.UserService;
 import co.edu.cedesistemas.common.DefaultResponseBuilder;
 import co.edu.cedesistemas.common.model.Status;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.hateoas.Link;
@@ -29,20 +29,25 @@ public class UserController {
     private final IUserService userService;
 
     @PostMapping("/users")
+    @HystrixCommand(fallbackMethod = "createUserFallback")
     public ResponseEntity<Status<?>> createUser(@RequestBody User user) {
         log.info("Creating user...");
         try {
             User created = this.userService.createUser(user);
             addSelfLink(created);
             addLinks(created);
+            log.info("The user was created successfully");
             return DefaultResponseBuilder.defaultResponse(created, HttpStatus.CREATED);
         } catch (Exception ex) {
+            log.error("There was a problem creating the user");
             return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping("/users/{id}")
+    @HystrixCommand(fallbackMethod = "getUserByIdFallback")
     public ResponseEntity<Status<?>> getUserById(@PathVariable String id) {
+        log.info("Getting the user by id");
         try {
             Optional<User> foundUser = this.userService.findById(id);
             if (foundUser.isPresent()) {
@@ -51,12 +56,15 @@ public class UserController {
             }
             return DefaultResponseBuilder.defaultResponse("User not found", HttpStatus.NOT_FOUND);
         } catch (Exception ex) {
+            log.error("There was a problem getting the user");
             return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping("/users/by-email")
+    @HystrixCommand(fallbackMethod = "getUsersByEmailFallback")
     public ResponseEntity<Status<?>> getUsersByEmail(@PathParam("email") String email) {
+        log.info("Getting the user by email...");
         try {
             List<User> users = this.userService.findUsersByEmail(email);
             if (users.size() > 0) {
@@ -65,38 +73,99 @@ public class UserController {
             }
             return DefaultResponseBuilder.defaultResponse("Users not found", HttpStatus.NOT_FOUND);
         } catch (Exception ex) {
+            log.error("There was an error getting the user by email");
             return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @DeleteMapping("/users/{id}")
+    @HystrixCommand(fallbackMethod = "deleteUserFallback")
     public ResponseEntity<Status<?>> deleteUser(@PathVariable String id) {
+        log.info("Deleting the user...");
         try {
             Optional<User> deletedUser = this.userService.deleteUserById(id);
 
             if (deletedUser.isPresent()) {
                 addSelfLink(deletedUser.get());
+                log.info("The user was deleted successfully");
                 return DefaultResponseBuilder.defaultResponse(deletedUser.get(), HttpStatus.OK);
             }
+            log.warn("The user couldn't be deleted");
             return DefaultResponseBuilder.defaultResponse("User no found", HttpStatus.NOT_FOUND);
         } catch (Exception ex) {
+            log.error("There was an error getting the user by email");
             return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PatchMapping("/users/{id}")
+    @HystrixCommand(fallbackMethod = "updateUserFallback")
     public ResponseEntity<Status<?>> updateUser(@PathVariable String id, @RequestBody User user) {
+        log.info("Updating the user...");
         try {
             Optional<User> updatedUser = this.userService.updateUser(id, user);
 
             if (updatedUser.isPresent()) {
                 addSelfLink(updatedUser.get());
+                log.info("The user was updated successfully");
                 return DefaultResponseBuilder.defaultResponse(updatedUser.get(), HttpStatus.OK);
             }
+            log.warn("The user couldn't be updated");
             return DefaultResponseBuilder.defaultResponse("User no found", HttpStatus.NOT_FOUND);
         } catch (Exception ex) {
+            log.error("There was a problem updating the user");
             return DefaultResponseBuilder.errorResponse(ex.getMessage(), ex, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    private ResponseEntity<Status<?>> createUserFallback(@RequestBody User user) {
+        log.error("Creating user fallback {}");
+
+        Status<?> status = Status.builder()
+                .message("Service unavailable. Please try again")
+                .code(HttpStatus.SERVICE_UNAVAILABLE.value())
+                .build();
+        return new ResponseEntity<>(status, HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    private ResponseEntity<Status<?>> getUserByIdFallback(@RequestBody User user) {
+        log.error("Getting user by id fallback {}");
+
+        Status<?> status = Status.builder()
+                .message("Service unavailable. Please try again")
+                .code(HttpStatus.SERVICE_UNAVAILABLE.value())
+                .build();
+        return new ResponseEntity<>(status, HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    private ResponseEntity<Status<?>> getUsersByEmailFallback(@RequestBody User user) {
+        log.error("Getting user by email fallback {}");
+
+        Status<?> status = Status.builder()
+                .message("Service unavailable. Please try again")
+                .code(HttpStatus.SERVICE_UNAVAILABLE.value())
+                .build();
+        return new ResponseEntity<>(status, HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    private ResponseEntity<Status<?>> deleteUserFallback(@RequestBody User user) {
+        log.error("Deleting user fallback {}");
+
+        Status<?> status = Status.builder()
+                .message("Service unavailable. Please try again")
+                .code(HttpStatus.SERVICE_UNAVAILABLE.value())
+                .build();
+        return new ResponseEntity<>(status, HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    private ResponseEntity<Status<?>> updateUserFallback(@RequestBody User user) {
+        log.error("Updating user fallback {}");
+
+        Status<?> status = Status.builder()
+                .message("Service unavailable. Please try again")
+                .code(HttpStatus.SERVICE_UNAVAILABLE.value())
+                .build();
+        return new ResponseEntity<>(status, HttpStatus.SERVICE_UNAVAILABLE);
     }
 
 
