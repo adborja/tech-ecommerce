05d9fe8766cad31494bcddb71dbcb8bee7b2070e
Jaime Barrera Mejia <jbarrera027@hotmail.com>
2020-07-27 13:18:55 +0000

    avances taller de graylog

---
+++ registration-service/src/main/java/co/edu/cedesistemas/commerce/registration/config/CommerceConfig.java
@@ -0,0 +1,16 @@
+package co.edu.cedesistemas.commerce.registration.config;
+
+import co.edu.cedesistemas.commerce.registration.model.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class CommerceConfig {
+    @Bean
+    public Map<String, User> getStoreMap() {
+        return new HashMap<>();
+    }
+}
