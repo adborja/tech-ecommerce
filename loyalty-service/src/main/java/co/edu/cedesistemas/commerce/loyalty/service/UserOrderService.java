61aad2aeea4ee4297be74c7bc6049bd2332c6aae
FedeOO <feedkiko@gmail.com>
2020-07-31 20:50:20 -0500

    inicio practica 10

--- loyalty-service/src/main/java/co/edu/cedesistemas/commerce/loyalty/service/UserOrderService.java
+++ loyalty-service/src/main/java/co/edu/cedesistemas/commerce/loyalty/service/UserOrderService.java
@@ -5,6 +5,10 @@ import co.edu.cedesistemas.commerce.loyalty.model.UserOrder;
 import co.edu.cedesistemas.commerce.loyalty.repository.UserOrderRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
@@ -15,6 +19,9 @@ import javax.validation.constraints.NotNull;
 public class UserOrderService {
     private final UserOrderRepository repository;
     private final EventPublisherService publisherService;
+    
+    @Value("${app.loyalty.points-conversion-rate}")
+    private final int pointsConversionRate;
 
     public UserOrder registerOrder(@NotNull final String orderId, @NotNull String userId, @NotNull Float orderValue) {
         UserOrder uo = new UserOrder();
@@ -22,8 +29,11 @@ public class UserOrderService {
         uo.setUserId(userId);
         uo.setStatus(LoyaltyStatus.REGISTERED);
         uo.setOrderValue(orderValue);
-        uo.calculatePoints();
+//        uo.calculatePoints(Integer.parseInt(pointsConversionRate));
 
+        uo.calculatePoints(pointsConversionRate);
+        log.info("conversion rate obtained from property {}",pointsConversionRate);
+        
         UserOrder result = repository.save(uo);
 
         publisherService.publishLoyaltyEvent(result);
