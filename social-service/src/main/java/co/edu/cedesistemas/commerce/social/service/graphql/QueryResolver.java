86a414aa25e693b0db755ecf022a01a307c97e02
yacalder <yacalder@bancolombia.com.co>
2020-08-05 18:14:50 -0500

    Adding GraphQL implementation - Taller 11

---
+++ social-service/src/main/java/co/edu/cedesistemas/commerce/social/service/graphql/QueryResolver.java
@@ -0,0 +1,39 @@
+package co.edu.cedesistemas.commerce.social.service.graphql;
+
+import co.edu.cedesistemas.commerce.social.model.User;
+import co.edu.cedesistemas.commerce.social.repository.StoreRepository;
+import co.edu.cedesistemas.commerce.social.service.StoreService;
+import co.edu.cedesistemas.commerce.social.service.UserService;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class QueryResolver implements GraphQLQueryResolver {
+    private final UserService userService;
+    private final StoreService storeService;
+
+    public User getUser(final String id){
+        return userService.getById(id);
+    }
+
+    public List<StoreRepository.StoreOccurrence> getRecommendStoreByProducts(final String userId, final String zone,
+                                                                             final String productType, final int limit){
+        return storeService.recommendStoreByProducts(userId, zone, productType, limit);
+    }
+
+    public List<StoreRepository.StoreOccurrence> getRecommendStoresByZoneAndProductType(final String userId,
+                                                                                        final String zone,
+                                                                                        final String productType,
+                                                                                        final int limit){
+        return storeService.recommendStoresByZoneAndProductType(userId, zone, productType, limit);
+    }
+
+    public List<StoreRepository.StoreOccurrence> getRecommendStoresByZone(final String userId, final String zone,
+                                                                          final int limit){
+        return storeService.recommendStoresByZone(userId,zone, limit);
+    }
+}
