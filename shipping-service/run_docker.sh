b91b35f24e131dad22dafe88885049f11a57b8d3
adborja <adborja@gmail.com>
2020-08-06 15:52:01 -0500

    Adding shipping service. Adding docker build tools.

--- shipping-service/run_docker.sh
+++ shipping-service/run_docker.sh
@@ -4,6 +4,6 @@ docker run \
   --hostname shipping.cedesistemas.local \
   --net cedesistemas_network \
   -e "SPRING_PROFILES_ACTIVE=sandbox" \
+  -e "eureka.client.serviceUrl.defaultZone=http://eureka.cedesistemas.local:8761/eureka" \
   -p 8086:8086 \
   -d registry.cedesistemas.internal:5000/commerce/shipping-service:dev
-
