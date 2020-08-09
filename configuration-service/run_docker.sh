1849de539b8931c44ecab5a7b10b5c67a20c1760
albertojigar <albertojigar@gmail.com>
2020-08-09 14:28:33 -0500

    Solucion: taller12_reto_spring_docker

---
+++ configuration-service/run_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker run \
+  --name=config.cedesistemas.local \
+  --hostname config.cedesistemas.local \
+  --net cedesistemas_network \
+  -v /Users/Beto/.ssh/:/root/.ssh/ \
+  -e "eureka.client.serviceUrl.defaultZone=http://discovery.cedesistemas.local:8761/eureka" \
+  -p 8760:8760 \
+  -d registry.cedesistemas.internal:5000/commerce/configuration-service:dev
+
+
---
+++ social-service/run_docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run \
+  --name=social.cedesistemas.local \
+  --hostname social.cedesistemas.local \
+  --net cedesistemas_network \
+  -e "eureka.client.serviceUrl.defaultZone=http://discovery.cedesistemas.local:8761/eureka" \
+  -p 8082:8082 \
+  -d registry.cedesistemas.internal:5000/commerce/social-service:dev
