9993f72f305868ac1cccf98e51621af629ca3f89
slotero <slotero@bancolombia.com.co>
2020-08-09 18:36:00 -0500

    Taller 10 ext configuration

--- loyalty-service/src/main/java/co/edu/cedesistemas/commerce/loyalty/service/UserOrderService.java
+++ loyalty-service/src/main/java/co/edu/cedesistemas/commerce/loyalty/service/UserOrderService.java
@@ -1,32 +1,39 @@
 package co.edu.cedesistemas.commerce.loyalty.service;
 
-import co.edu.cedesistemas.common.model.LoyaltyStatus;
 import co.edu.cedesistemas.commerce.loyalty.model.UserOrder;
 import co.edu.cedesistemas.commerce.loyalty.repository.UserOrderRepository;
-import lombok.AllArgsConstructor;
+import co.edu.cedesistemas.common.model.LoyaltyStatus;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
+@RefreshScope
 @Slf4j
 public class UserOrderService {
     private final UserOrderRepository repository;
     private final UserStoreService userStoreService;
     private final EventPublisherService publisherService;
 
+    @Value("${app.loyalty.points-conversion-rate:200}")
+    private Integer pointsConversionRate;
+
     public UserOrder registerOrder(@NotNull final String orderId, @NotNull final String storeId,
                                    @NotNull String userId, @NotNull Float orderValue) {
+        log.info("pointsConversionRate..{}",pointsConversionRate);
         UserOrder uo = new UserOrder();
         uo.setId(orderId);
         uo.setStoreId(storeId);
         uo.setUserId(userId);
         uo.setStatus(LoyaltyStatus.REGISTERED);
         uo.setOrderValue(orderValue);
-        uo.calculatePoints();
+        uo.calculatePoints(pointsConversionRate);
 
         userStoreService.updatePoints(storeId, userId, uo.getPoints());
 
