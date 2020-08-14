05cd6f1b7abb3f6ace917427e1ab8458f51c281c
adborja <adborja@gmail.com>
2020-08-14 16:18:33 -0500

    Adding reactive service cart-service

---
+++ cart-service/src/main/java/co/edu/cedesistemas/commerce/cart/config/WebFluxConfig.java
@@ -0,0 +1,10 @@
+package co.edu.cedesistemas.commerce.cart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebFluxConfig implements WebFluxConfigurer {
+}
