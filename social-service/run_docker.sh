6c4a4f5cb1a9dd92f9f441d49478d23af1674752
Gabriel Galeano <gabrieljgm@gmail.com>
2020-08-14 18:31:02 -0500

    Se adiciona --add-host a run_docker.sh de social service

--- social-service/run_docker.sh
+++ social-service/run_docker.sh
@@ -4,5 +4,6 @@ docker run \
   --hostname social.cedesistemas.local \
   --net cedesistemas_network \
   -e "eureka.client.serviceUrl.defaultZone=http://discovery.cedesistemas.local:8761/eureka" \
+  --add-host "neo4j.cedesistemas.local:192.168.0.131" \
   -p 8082:8082 \
   -d registry.cedesistemas.internal:5000/commerce/social-service:dev
