c912eaa5e6617a8409508090678129bc6a84fd31
kathe20m <kathe20m@hotmail.com>
2020-08-16 18:46:09 -0500

    taller 13 docker-compose

--- configuration-service/run_docker.sh
+++ configuration-service/run_docker.sh
@@ -3,7 +3,7 @@ docker run \
   --name=config.cedesistemas.local \
   --hostname config.cedesistemas.local \
   --net cedesistemas_network \
-  -v ~/.ssh/:/root/.ssh/ \
+  -v pabliny/.ssh/:/root/.ssh/ \
   -e "eureka.client.serviceUrl.defaultZone=http://eureka.cedesistemas.local:8761/eureka" \
   -p 8760:8760 \
   -d registry.cedesistemas.internal:5000/commerce/configuration-service:dev
