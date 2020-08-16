ca9810e136cb010a29ff86392bba0f1359899ff4
Edward Alexis Ortiz <alexixsortizz@gmail.com>
2020-08-16 08:57:55 -0500

    taller de docker compose

---
+++ loyalty-service/run_build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p target/dependency
+unzip build/libs/loyalty-service-0.0.1-SNAPSHOT.jar -d target/dependency/
+docker build -t registry.cedesistemas.internal:5000/commerce/loyalty-service:dev .
---
+++ payment-service/run_build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p target/dependency
+unzip build/libs/payment-service-0.0.1-SNAPSHOT.jar -d target/dependency
+docker build -t registry.cedesistemas.internat:5000/commerce/payment-service:dev .
