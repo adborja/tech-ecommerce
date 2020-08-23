52f2607c13802bd6b5c162b72c8d57f84165de8f
Jaime Barrera Mejia <JABARRER@bancolombia.com.co>
2020-08-23 17:03:02 -0500

    Ajustes finales a inventory-service

--- inventory-service/src/main/java/co/edu/cedesistemas/commerce/inventory/InventoryApp.java
+++ inventory-service/src/main/java/co/edu/cedesistemas/commerce/inventory/InventoryApp.java
@@ -3,7 +3,7 @@ package co.edu.cedesistemas.commerce.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+//ajuste inventory
 @SpringBootApplication
 @EnableDiscoveryClient
 public class InventoryApp {
