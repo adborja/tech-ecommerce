9c86926dba88dde6c7ea753c8c1dd92bacef0a49
Jaime Barrera Mejia <JABARRER@bancolombia.com.co>
2020-08-23 17:11:35 -0500

    Ajustes Registration para el EventPublisher

--- registration-service/src/main/java/co/edu/cedesistemas/commerce/registration/service/EventPublisherService.java
+++ registration-service/src/main/java/co/edu/cedesistemas/commerce/registration/service/EventPublisherService.java
@@ -11,7 +11,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-
+//jaime
 @Service
 @AllArgsConstructor
 @Slf4j
