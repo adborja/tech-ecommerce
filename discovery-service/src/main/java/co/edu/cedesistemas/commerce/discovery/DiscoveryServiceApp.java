6b51aac3655cceb6ce012c203f277a797ca64511
Jaime Barrera Mejia <JABARRER@bancolombia.com.co>
2020-08-23 16:58:51 -0500

    Taller Final –  SpringBoot and Microservices

--- discovery-service/src/main/java/co/edu/cedesistemas/commerce/discovery/DiscoveryServiceApp.java
+++ discovery-service/src/main/java/co/edu/cedesistemas/commerce/discovery/DiscoveryServiceApp.java
@@ -3,7 +3,7 @@ package co.edu.cedesistemas.commerce.discovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//jaime
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServiceApp {
