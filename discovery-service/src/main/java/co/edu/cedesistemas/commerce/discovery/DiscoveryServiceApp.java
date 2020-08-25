8445b6e39689fe0c0f31d9c24c2e39903ba57601
Jaime Barrera Mejia <JABARRER@bancolombia.com.co>
2020-08-25 11:13:57 -0500

    Taller 12

--- discovery-service/src/main/java/co/edu/cedesistemas/commerce/discovery/DiscoveryServiceApp.java
+++ discovery-service/src/main/java/co/edu/cedesistemas/commerce/discovery/DiscoveryServiceApp.java
@@ -3,7 +3,7 @@ package co.edu.cedesistemas.commerce.discovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-//jaime
+//import lombok
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServiceApp {
